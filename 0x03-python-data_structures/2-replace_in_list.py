34794060e14ff45659cd8f4053ece66e5c9e4638
yusuf <yusufkolu12345@gmail.com>
2022-12-03 01:35:46 -0800

    ii

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    lenght = len(my_list)
+    if (idx < 0 or idx > lenght):
+        return my_list
+    else:
+        return my_list
