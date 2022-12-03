bf70d5fb52d5ed4d128c674085fb5c376131d3bc
yusuf <yusufkolu12345@gmail.com>
2022-12-03 13:56:56 -0800

    iajnnjd

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,3 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     my = reversed(my_list)
     for a in my:
         print("{:d}".format(a))
+
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in aatrix:
+        for val in row:
+            print(val, end=" ")
+        print()
